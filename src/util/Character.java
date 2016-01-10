1ec1ed462e7fc27a89dc9731c8e0c31d4001f318
Alan Johnson <igeryu@gmail.com>
2016-01-10 14:07:49 -0700

    Added new methods and improved existing ones in ProfileManager and Character, updated Subsystem (UML)

--- src/util/Character.java
+++ src/util/Character.java
@@ -5,6 +5,7 @@
  */
 package util;
 
+import components.Profile;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,4 +15,55 @@ import java.util.HashMap;
  */
 public class Character {
     private HashMap <String, ArrayList<Backup>> profileBackups;
+    private String name;
+    private String id;
+    
+    
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getName() {
+        return name;
+    }
+    
+    
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getId() {
+        return id;
+    }
+    
+    
+    
+    /**
+     * TODO:  Fill this in.
+     * 
+     * Loads profile by asking user to select one of the profile backups of held
+     * by this character object.
+     * 
+     * @return     Latest profile backup of the profile selected by the user.
+     */
+    public Profile loadProfile () {
+        
+        //  TODO:  Ask user to select the profile they want to load
+        
+        /**
+         *   TODO:  Ask the user if they want the latest backup of that profile,
+         *          or an older one.
+         */
+        
+        /**
+         * TODO:  Load the profile selected by the user into memory and
+         *        overwrite the active profile folder files with those in the
+         *        backup folder.
+        */
+        
+        return null;
+    }
+    
 }
